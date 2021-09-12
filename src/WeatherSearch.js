739b81125c4e6c8710784871f9c9eff22b753fff
TreeLife8 <85816400+TreeLife8@users.noreply.github.com>
2021-09-12 22:06:22 +1000

    added a react loader for when the api call is being rendered and added weather state object for weather data variables

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -2,13 +2,24 @@ import React, { useState } from "react";
 import "./WeatherSearch.css";
 import DateTime from "./DateTime";
 import axios from "axios";
+import Loader from "react-loader-spinner";
+import Weather from "./Weather";
 
 export default function WeatherSearch() {
   const [weatherData, setWeatherDate] = useState({ ready: false });
-  function handleResponse(event) {
+
+  function handleResponse(response) {
     setWeatherDate({
       ready: true,
-      // city:
+      city: response.data.name,
+      temperature: Math.round(response.data.main.temp),
+      wind: response.data.wind.speed,
+      feelsLike: Math.round(response.data.main.feels_like),
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      maxTemp: Math.round(response.data.main.temp_max),
+      minTemp: Math.round(response.data.main.temp_min),
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
     console.log(response.data);
   }
@@ -44,11 +55,28 @@ export default function WeatherSearch() {
           </div>
           <div className="col-6"></div>
         </div>
+        <Weather city={weatherData.city} />
       </div>
     );
   } else {
     let apiKey = `3fb188379e6ffcf616e7cdbd010c6434`;
+    let city = `sydney`;
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
+    return (
+      <div className="container row">
+        <div className="col-5"></div>
+        <div className="col-4">
+          <Loader
+            type="Puff"
+            color="#757a79"
+            height={200}
+            width={200}
+            timeout={4000}
+          />
+        </div>
+        <div className="col-3"></div>
+      </div>
+    );
   }
 }
