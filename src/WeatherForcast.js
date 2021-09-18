f93a76bd4fd1fd9efdf63463758e30f99e6a8e59
TreeLife8 <85816400+TreeLife8@users.noreply.github.com>
2021-09-18 14:28:13 +1000

    Updated axios call for forcast

--- src/WeatherForcast.js
+++ src/WeatherForcast.js
@@ -1,34 +1,41 @@
-import React, { useState } from "react";
-import "./WeatherForcast.css";
-import WeatherIcon from "./WeatherIcon";
-import axios from "axios";
+// import React, { useState } from "react";
+// import "./WeatherForcast.css";
+// import WeatherIcon from "./WeatherIcon";
+// import axios from "axios";
 
-export default function WeatherForcast(props) {
-  const [forcastData, setForcastData] = useState("");
-  // function handleResponse(response) {
-  //   console.log(response.data.daily[0].temp.max);
-  //   setForcastData({
-  //     maxTemp: Math.round(response.data.daily[0].temp.max),
-  //     minTemp: Math.round(response.data.daily[0].temp.min),
-  //   });
-  // }
-  // let apiKey = "2f5ed0987c11d8af0a71b4472673fde7";
-  // // let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${props.lat}&lon=${props.lon}&appid=${apiKey}&units=${props.unit}`;
-  // axios.get(apiUrl).then(handleResponse);
-  return (
-    <div className="WeatherForcast">
-      <div className="row">
-        <div className="col-2">
-          <div className="day">Thu</div>
-          <div className="WeatherForcast-icon">
-            <WeatherIcon code="01d" size={35} />
-          </div>
-          <div className="temperatures">
-            <span className="max-temp">{forcastData.maxTemp}</span>°
-            <span className="min-temp">{forcastData.minTemp}°</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+// export default function WeatherForcast(props) {
+//   const [forcastData, setForcastData] = useState("");
+//   function handleResponse(response) {
+//     console.log(response.data.daily[0].temp.max);
+//     setForcastData({
+//       maxTemp: Math.round(response.data.daily[0].temp.max),
+//       minTemp: Math.round(response.data.daily[0].temp.min),
+//     });
+//   }
+//   function search() {
+//     let apiKey = "2f5ed0987c11d8af0a71b4472673fde7";
+//     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${props.lat}&lon=${props.lon}&appid=${apiKey}&units=${props.unit}`;
+//     axios.get(apiUrl).then(handleResponse);
+//   }
+//   if (forcastData.ready) {
+//     return (
+//       <div className="WeatherForcast">
+//         <div className="row">
+//           <div className="col-2">
+//             <div className="day">Thu</div>
+//             <div className="WeatherForcast-icon">
+//               <WeatherIcon code="01d" size={35} />
+//             </div>
+//             <div className="temperatures">
+//               <span className="max-temp">{forcastData.maxTemp}</span>°
+//               <span className="min-temp">{forcastData.minTemp}°</span>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   } else {
+//     search();
+//     return <div>Loading...</div>;
+//   }
+// }
